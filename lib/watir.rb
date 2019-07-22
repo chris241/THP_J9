c57c873c5d54ce090f7924f3755c0470a2f8777e
chris <chrisleon325@gmail.com>
2019-07-22 15:40:42 +0300

    maj1 watir

--- lib/watir.rb
+++ lib/watir.rb
@@ -13,7 +13,7 @@ def search (a)
 	browser = Watir::Browser.new(:firefox)
 	browser.goto 'google.com'
 	search_bar = browser.text_field(class:'gsfi')
-	search_bar.set (a)
+	search_bar.set (a.join(" "))
 	search_bar.send_keys(:enter)
 	submit_button = browser.button(type:"submit")
 	submit_button.click
