e5e33db50e2617e1c6b34e0498fbcff41301cb5c
chris <chrisleon325@gmail.com>
2019-07-22 11:43:13 +0300

    mise à jour du watir.rf

--- lib/watir.rb
+++ lib/watir.rb
@@ -1,17 +1,19 @@
 require 'watir'
 require 'selenium-webdriver'
 
-def check_if_user_gave_input  (a)
-  abort("search: missing input") if ARGV.empty?
-
-  return check_if_user_gave_input 
-end
-
+def check_if_user_gave_input 
+	a = ARGV 
+  	if a.empty?
+  		puts "search: missing input"
+  	else
+  		return a
+  	end
+  end
 def search (a)
 	browser = Watir::Browser.new(:firefox)
 	browser.goto 'google.com'
 	search_bar = browser.text_field(class:'gsfi')
-	search_bar.set ()
+	search_bar.set (a)
 	search_bar.send_keys(:enter)
 	submit_button = browser.button(type:"submit")
 	submit_button.click
@@ -19,8 +21,7 @@ def search (a)
 end
 
 def perform
-	check_if_user_gave_input 
-	search (a)
+	a = check_if_user_gave_input
+	search(a)
 end
-
 perform
